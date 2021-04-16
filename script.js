21cbd076c3e190950d3a6cb434b785cfd8de2664
Nick <sytnyk.m@ukr.net>
2021-04-16 19:23:14 +0300

    циклы

--- script.js
+++ script.js
@@ -1,8 +1,36 @@
 "use strict";
 
-const obj = {
-    name:"Nick",
-    age:25
+const numberOfFilms = +prompt("Сколько фильмов Вы уже просмотрели?", "");
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
 };
 
-console.log(obj.name);
+for (let i = 0; i < 2; i++) {
+    const a = prompt("Один из последних просмотренных фильмов?", ""),
+          b = prompt("На сколько оцените его?", "");
+
+    if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log ("done");
+    } else{
+        console.log ("Error");
+        i--;
+    }
+}
+
+if (personalMovieDB.count < 10){
+    console.log ("Мало");
+} else if(personalMovieDB.count > 10){
+    console.log ("Нормально");        
+} else {
+    console.log ("Много");  
+}
+
+
+
+console.log(personalMovieDB);
+
